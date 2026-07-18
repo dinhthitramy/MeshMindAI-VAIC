ca056d21591bf67f73eac30500cfd75aa0398d5f
vinh vu <vdqvinh2004@gmail.com>
2026-07-18 17:05:33 +0700

    hotfix: docker compose file and merge all db migration

--- drizzle/0002_backfill_user_roles.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO "user_roles" ("user_id", "role_id")
-SELECT "users"."id", "roles"."id"
-FROM "users"
-CROSS JOIN "roles"
-WHERE "roles"."key" = 'USER'
-ON CONFLICT ("user_id", "role_id") DO NOTHING;
--- drizzle/0004_magenta_lily_hollister.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "oauth_accounts" ADD COLUMN "access_token" text;--> statement-breakpoint
-ALTER TABLE "oauth_accounts" ADD COLUMN "refresh_token" text;--> statement-breakpoint
-ALTER TABLE "oauth_accounts" ADD COLUMN "token_expires_at" timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "oauth_accounts" ADD COLUMN "updated_at" timestamp with time zone DEFAULT now() NOT NULL;
